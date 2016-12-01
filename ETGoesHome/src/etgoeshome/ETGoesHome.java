80667f9b5581b899d7702eac84461c42abb61542
Daniel <Daniel@ThinkyPad>
2016-11-30 19:16:21 -0800

    Initial Commit.

---
+++ ETGoesHome/src/etgoeshome/ETGoesHome.java
@@ -0,0 +1,16 @@
+package etgoeshome;
+
+/**
+ *
+ * @author Daniel Yao
+ */
+public class ETGoesHome {
+
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+      // TODO code application logic here
+   }
+   
+}
