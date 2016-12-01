57bdb40b263aa5324eabfd23fe37d8d6cdc4f93d
Daniel <Daniel@ThinkyPad>
2016-11-30 19:52:06 -0800

    Added create tables sql file

---
+++ ETGoesHome/createTables.sql
@@ -0,0 +1,42 @@
+/**
+ * Author:  Daniel
+ * Created: Nov 30, 2016
+ */
+
+CREATE TABLE Location (
+   Id INT,
+   State CHAR(2),
+   City VARCHAR(50),
+   PRIMARY KEY (State, City)
+);
+
+CREATE TABLE UFOSightings (
+   Occurence DATETIME,
+   LocationId INT,
+   State CHAR(2),
+   City VARCHAR(50),
+   Shape VARCHAR(15),
+   Duration_Seconds INT,
+   Description VARCHAR(200),
+   PRIMARY KEY(Occurence, City, State),
+   FOREIGN KEY(State, City) REFERENCES Location(State, City)
+);
+
+CREATE TABLE Shootings (
+   Id INT,
+   Name VARCHAR(50),
+   Day DATE,
+   Death VARCHAR(20),
+   Weapon VARCHAR(15),
+   Age INT,
+   Gender CHAR(1),
+   Race CHAR(1),
+   City VARCHAR(50),
+   State CHAR(2),
+   Mental CHAR(1),
+   Threat VARCHAR(15),
+   Flee VARCHAR(15),
+   BodyCamera CHAR(1),
+   PRIMARY KEY(Id),
+   FOREIGN KEY(State, City) REFERENCES Location(State, City)
+);
